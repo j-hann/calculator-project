510c448cdaea26dcfc83cffeda4baeb62255a721
j-hann <j.han77055@gmail.com>
2024-10-08 15:53:44 +0900

    first commit

---
+++ src/com/example/calculator/calculator.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+public class calculator {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Calculator!");
+
+    }//main
+}//end class
+
