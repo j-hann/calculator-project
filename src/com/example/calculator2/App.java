066e5a669bb8d77f9e7f833ae45092302441ed82
j-hann <j.han77055@gmail.com>
2024-10-17 09:27:08 +0900

    read me 작성 진행중

---
+++ src/com/example/calculator2/App.java
@@ -0,0 +1,14 @@
+package com.example.calculator2;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        //객체 생성
+        Calculator cal = new Calculator();//Calculator 클래스의 객체인 calculator 인스턴스 생성
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }//main
+}//end class
--- src/com/example/calculator2/Calculator.java
+++ src/com/example/calculator2/Calculator.java
@@ -1,10 +1,24 @@
 package com.example.calculator2;
 
 public class Calculator {
+//연산 수행 역할 클래스
+//App 클래스 활용
 
-    public static void main(String[] args){
-        System.out.println("Hello, Calculator!");
+//            1. 만들려고 하는 설계도를 선언합니다.(클래스 선언)
+//            2. 객체가 가지고 있어야 할 속성(필드)을 정의합니다.
+//            3. 객체를 생성하는 방식을 정의합니다.(생성자)
+//            4. 객체가 가지고 있어야 할 행위(메서드)를 정의합니다.
 
-    }//main
+
+
+    //속성(필드)
+//    int result =
+    
+    //생성자
+
+    //행위(기능)메서드
+    
+
+
 }//end class
 
